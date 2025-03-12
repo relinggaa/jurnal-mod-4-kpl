21b5e62e3d208d04cc98fa2eead85dc9f552b9b8
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:27:55 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300107
+{
+    class KodeProduk
+    {
+        public string getKodeProduk(string produk)
+        {
+            Dictionary<string, string> kodeProduk = new Dictionary<string, string>
+            {
+                {"Laptop","E100" },
+                {"Smartpgone","E101" },
+                {"Tablet","E102" },
+                {"Headset","E103" },
+                {"Keyboard","E104" },
+                {"Mouse","E105" },
+                {"Printer","E106" },
+                {"Monitor","E107" },
+                {"Smartwatch","E108" },
+                {"Kamera","E109" }
+
+            };
+            return  kodeProduk.ContainsKey(produk) ? kodeProduk[produk] : "Kode produk tidak ditemukan";
+        }
+  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Mail;
+using System.Reflection.Metadata;
+using modul4_103022300107;
+
+while (true)
+{
+    Console.WriteLine("Pilih Menu");
+    Console.WriteLine("1.Kode Produk");
+    Console.WriteLine("2.Keluar");
+
+    //string pilihan = Console.ReadLine();
+
+    //switch (pilihan)
+    //{
+    //    case "1":
+
+    //        HandleKodeProduk();
+    //    default:
+    //        Console.WriteLine("Pilihan Tidak Valid");
+    //        break;
+    //}
+    HandleKodeProduk();
+}
+static void HandleKodeProduk()
+{
+    KodeProduk kodeProduk = new KodeProduk();
+    Console.Write("Masukan Nama Produk :");
+    string produk = Console.ReadLine();
+    string kode = kodeProduk.getKodeProduk(produk);
+    Console.WriteLine($"Kode produk untuk {produk} adalah {kode}");
+}
